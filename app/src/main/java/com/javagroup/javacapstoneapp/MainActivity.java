6ed81a1c659dbfb3112e7bcb41270a649f09eb02
Sagar Soni <s.soni247@mybvc.ca>
2020-01-28 13:03:21 -0700

    created page of Human Rights with including link

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.javagroup.javacapstoneapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text= (TextView) findViewById(R.id.textlink);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView t= (TextView) findViewById(R.id.human_rights_complainght);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
