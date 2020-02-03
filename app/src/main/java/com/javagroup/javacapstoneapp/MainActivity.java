f8b956cf3a7f6101abfd0c5f830db5d6da10dec5
victor charl <corpuzvictorcharl@gmail.com>
2020-02-03 11:34:49 -0700

    initial layout for ohs

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -2,11 +2,15 @@ package com.javagroup.javacapstoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sayHello(View view) {
-        Toast.makeText(this, "You've just clicked the button", Toast.LENGTH_SHORT).show();
+    public void linkPage(View view) {
+        goToUrl ("https://workershealthcentre.ca/4-health-and-safety-rights/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void goOhsLink(View view) {
+        goToUrl ("https://www.alberta.ca/ohs-act-regulation-code.aspx");
     }
 }
