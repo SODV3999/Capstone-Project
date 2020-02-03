526fc05b63ab157c5c9c5086ae6164390210cc9c
Twinkle Patel <t.patel457@mybvc.ca>
2020-02-02 18:48:47 -0700

    add functionality for call description : need to check this code

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.javagroup.javacapstoneapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView text= (TextView) findViewById(R.id.textlink);
+        TextView text = (TextView) findViewById(R.id.textlink);
         text.setMovementMethod(LinkMovementMethod.getInstance());
 
-        TextView t= (TextView) findViewById(R.id.Resource_Center);
+        TextView t = (TextView) findViewById(R.id.Resource_Center);
         t.setMovementMethod(LinkMovementMethod.getInstance());
 
+        Button dial = findViewById(R.id.Dial1);
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void OnDialButton(View v) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:7804869009"));
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        startActivity(intent);
     }
 }
