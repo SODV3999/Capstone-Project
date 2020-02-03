16b1b6124ace28754d16d0753bdeb14ff3cfee5f
Bruno Mendonca <me@brunomendonca.com>
2020-02-03 14:21:02 -0700

    front page with navigation button

---
+++ app/src/main/java/com/javagroup/javacapstoneapp/ESActivity.java
@@ -0,0 +1,22 @@
+package com.javagroup.javacapstoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class ESActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_es);
+        TextView text = (TextView) findViewById(R.id.textlink);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t = (TextView) findViewById(R.id.employmentcode);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView te = (TextView) findViewById(R.id.EmploymentRegulations);
+        te.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
