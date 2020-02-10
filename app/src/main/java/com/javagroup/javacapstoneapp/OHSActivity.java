4217197af09cd4cb936386c20ea6caeadc785087
victor charl <corpuzvictorcharl@gmail.com>
2020-02-10 12:18:18 -0700

    embedded browser

--- app/src/main/java/com/javagroup/javacapstoneapp/OHSActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/OHSActivity.java
@@ -1,16 +1,26 @@
 package com.javagroup.javacapstoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.net.Uri;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class OHSActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView ohsActAndCodeLink, safetyRightsLink;
+    WebView browser;
+    Button closeBrowser;
+
+    ConstraintLayout browserScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +30,25 @@ public class OHSActivity extends AppCompatActivity implements View.OnClickListen
         ohsActAndCodeLink = (TextView)findViewById(R.id.ohsActRegAndCodeLink);
         safetyRightsLink = (TextView)findViewById(R.id.safetyRightsLink);
 
+        browser = (WebView)findViewById(R.id.webView);
+
+        closeBrowser = (Button)findViewById((R.id.closebrowser));
+
+        browserScreen = (ConstraintLayout)findViewById(R.id.browserScreen);
+
         ohsActAndCodeLink.setOnClickListener(this);
         safetyRightsLink.setOnClickListener(this);
+        closeBrowser.setOnClickListener(this);
+
 
     }
 
-    private void openingLink (String url) {
-        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(launchBrowser);
+    private void openingLink(String url){
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        //startActivity(intent);
+        browser.setWebViewClient(new WebViewClient());
+        browser.loadUrl(url);
+
     }
 
     @Override
@@ -35,10 +56,19 @@ public class OHSActivity extends AppCompatActivity implements View.OnClickListen
         switch (v.getId()){
             case R.id.ohsActRegAndCodeLink:
                 openingLink ("https://workershealthcentre.ca/4-health-and-safety-rights/");
+                browserScreen.setVisibility(View.VISIBLE);
                 break;
             case R.id.safetyRightsLink:
                 openingLink ("https://www.alberta.ca/ohs-act-regulation-code.aspx");
+                browserScreen.setVisibility(View.VISIBLE);
                 break;
+            case R.id.closebrowser:
+                if(browserScreen.getVisibility() == View.VISIBLE)
+                {
+                    browserScreen.setVisibility(View.INVISIBLE);
+                }else{
+                    browserScreen.setVisibility(View.VISIBLE);
+                }
         }
     }
 }
