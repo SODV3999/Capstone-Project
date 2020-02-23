4448be29c9d1c37f929f15ec908999606bdac2be
Sagar Soni <s.soni247@mybvc.ca>
2020-02-23 14:15:00 -0700

    created add to contact and call button in humuna rights page

--- app/src/main/java/com/javagroup/javacapstoneapp/HumanRightsActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/HumanRightsActivity.java
@@ -1,13 +1,23 @@
 package com.javagroup.javacapstoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HumanRightsActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CALL = 1;
+        Button button_hs_dial_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +28,40 @@ public class HumanRightsActivity extends AppCompatActivity {
         TextView t= (TextView) findViewById(R.id.human_rights_complainght);
         t.setMovementMethod(LinkMovementMethod.getInstance());
 
+        button_hs_dial_1 = findViewById(R.id.button_hs_dial);
+        button_hs_dial_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity1();
+            }
+        });
+
+
+    }
+    public void Activity1(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+        }else{
+            // String dial ="tel:" +5879692301;
+            //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            String dial1 ="7804274952";
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:"+dial1));
+            startActivity(intent);
+        }
+
+
+
+    }
+
+    public void hs_contact_1(View view) {
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        Button button_hs_1 = findViewById(R.id.button_hs_contact1);
+        intent
+                .putExtra(ContactsContract.Intents.Insert.PHONE,button_hs_1.getText())
+                .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
+        startActivity(intent);
     }
 }
