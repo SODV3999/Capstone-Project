1aa4521598fadfc0c8185c8f594104a3ac7f4ab1
victor charl <corpuzvictorcharl@gmail.com>
2020-03-02 13:51:44 -0700

    splash screen implementation

--- app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView logo;
+
+    TextView school;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        logo = (ImageView)findViewById(R.id.imageView);
+        school = (TextView)findViewById(R.id.school_text);
+
         Thread th = new Thread(){
             public void run(){
                 try{
                     sleep(5000);
+
                 }
                 catch (Exception e){
                     e.printStackTrace();
