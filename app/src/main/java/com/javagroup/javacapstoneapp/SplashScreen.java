56f4761ed6dbc12a59c15ae37055b6be80ee2443
victor charl <corpuzvictorcharl@gmail.com>
2020-03-07 22:25:58 -0700

    disclaimer dialog

--- app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
@@ -1,16 +1,26 @@
 package com.javagroup.javacapstoneapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    ImageView logo;
+    Boolean firstTime = true;
 
+    ImageView logo;
     TextView school;
 
     @Override
@@ -18,12 +28,47 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        logo = (ImageView)findViewById(R.id.imageView);
-        school = (TextView)findViewById(R.id.school_text);
+        logo = (ImageView) findViewById(R.id.imageView);
+        school = (TextView) findViewById(R.id.school_text);
 
-        logo.animate().translationY(-200).setDuration(1000);
+        if(firstTime){
+            showDisclaimerDialog();
+        }
+        else{
+        }
+
+    }
+
+    public void showDisclaimerDialog(){
+        final Dialog disclaimerDialog = new Dialog(this);
+        disclaimerDialog.setContentView(R.layout.dialog_disclaimer);
+        Button btn_agree = (Button)disclaimerDialog.findViewById(R.id.btn_agree);
+        ImageButton btn_close = (ImageButton) disclaimerDialog.findViewById(R.id.btn_close);
+        final ImageView iv_logo = (ImageView)disclaimerDialog.findViewById(R.id.iv_logo);
+        btn_agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTime = false;
+                performSplashScreen();
+                disclaimerDialog.hide();
+            }
+        });
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        disclaimerDialog.setCancelable(false);
+        disclaimerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        disclaimerDialog.show();
+    }
+
+    public void performSplashScreen(){
+
+        logo.animate().alpha(1.0f).setDuration(1000);
+        logo.animate().translationY(-200).setDuration(1500);
         school.animate().alpha(1.0f).setDuration(1000);
-        school.animate().translationY(-200).setDuration(2500);
+        school.animate().translationY(-300).setDuration(2500);
 
         Thread th = new Thread(){
             public void run(){
