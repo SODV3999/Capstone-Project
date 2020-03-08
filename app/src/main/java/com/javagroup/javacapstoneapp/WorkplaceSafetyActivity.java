7be91cc7cc905331b91e85e7ff7edf28c44c2e49
Neil John Medalla <medallaneiljohn@gmail.com>
2020-03-07 23:09:04 -0700

    remove floating button

--- app/src/main/java/com/javagroup/javacapstoneapp/WorkplaceSafetyActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/WorkplaceSafetyActivity.java
@@ -14,9 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 public class WorkplaceSafetyActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -27,17 +25,6 @@ public class WorkplaceSafetyActivity extends AppCompatActivity implements Naviga
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
