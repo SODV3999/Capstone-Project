886364bf41a7f17a6d0e5192811c07b030708b0b
Neil John Medalla <medallaneiljohn@gmail.com>
2020-03-27 04:36:24 -0600

    Refactored nav click to not crash when clicked fast twice

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -28,25 +28,32 @@ public class MainActivity extends AppCompatActivity {
         navigationScreen = (ConstraintLayout)findViewById(R.id.navigationScreen);
         openNav = (ImageButton)findViewById(R.id.openNav);
 
+        // TODO: Research on how to use and implement threads properly
         openNav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                new Thread(new Runnable() {
+                Thread navThread = new Thread() {
                     @Override
                     public void run() {
-                        navigationScreen.setTranslationY(-3000);
-                        navigationScreen.setTranslationX(-3000);
-                        navigationScreen.animate().translationYBy(3000)
-                                .translationXBy(3000).setDuration(450);
-                        final FragmentTransaction openTheBrowser = getSupportFragmentManager()
-                                .beginTransaction();
-                        final NavigationActivity navigationActivity = new NavigationActivity();
-                        openTheBrowser.add(R.id.navigationScreen, navigationActivity);
-                        openTheBrowser.commit();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                navigationScreen.setTranslationY(-3000);
+                                navigationScreen.setTranslationX(-3000);
+                                navigationScreen.animate().translationYBy(3000)
+                                        .translationXBy(3000).setDuration(450);
+                                final FragmentTransaction openTheBrowser =
+                                        getSupportFragmentManager().beginTransaction();
+                                final NavigationActivity navigationActivity =
+                                        new NavigationActivity();
+                                openTheBrowser.add(R.id.navigationScreen, navigationActivity);
+                                openTheBrowser.commit();
+                            }
+                        });
                     }
-                }).start();
-
+                };
+                navThread.start();
             }
         });
 
