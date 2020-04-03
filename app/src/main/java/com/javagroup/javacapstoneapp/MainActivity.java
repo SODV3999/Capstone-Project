773e8a840a330bf6cb21708f1be092b883767f97
Nigam Budhwar <nigambudhwar@gmail.com>
2020-04-02 23:21:55 -0600

    created xml layout for Employment standards Done by Sagar and Nigam

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -97,4 +98,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this,
                 CaseStudy3.class));
     }
+
 }
