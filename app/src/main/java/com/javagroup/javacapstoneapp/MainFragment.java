de4e99c42e922560a326d7e49419ed3ed96b833d
Neil John Medalla <medallaneiljohn@gmail.com>
2020-04-09 18:23:00 -0600

    fixed retrieving strings from database

--- app/src/main/java/com/javagroup/javacapstoneapp/MainFragment.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainFragment.java
@@ -11,8 +11,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainFragment extends Fragment {
     int mainResource;
+    private View view;
+    private static final String TAG = "MainFragment";
+    private DatabaseReference ref = FirebaseOperations.navigation();
+
+    private TextView workplaceSafety, workPlaceSafetyIntro;
+    private TextView findYourVoice,
+            findYourVoiceIntro,
+            findYourVoiceBtnOne,
+            findYourVoiceBtnTwo,
+            findYourVoiceBtnThree;
+    private TextView resources,
+            resourcesIntro,
+            resourcesAlbertaTitle,
+            resourcesCalgaryWorkersTitle,
+            resourcesContentContinuation;
+
+    private String title, introduction;
+    private String findYourVoiceBtnOneString,
+            findYourVoiceBtnTwoString,
+            findYourVoiceBtnThreeString;
+    private String resourcesAlbertaString,
+            resourcesCalgaryWorkersString,
+            resourcesContentContinuationString;
 
     public MainFragment() {
     }
@@ -22,31 +50,115 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(mainResource, container, false);
+        view = inflater.inflate(mainResource, container, false);
 
         if (view.findViewById(R.id.main_section_workplace_safety) != null) {
-            TextView workplaceSafety = view.findViewById(R.id.title_WorkplaceSafety);
-            String message = "";
-            if (getArguments() != null) {
-                message = getArguments().getString("test");
-            }
-            workplaceSafety.setText(message);
+            getStringsForWorkplaceSafety();
         } else if (view.findViewById(R.id.main_btm_sheet_finding_your_voice) != null) {
-            TextView findYourVoice = view.findViewById(R.id.btm_sheet_title_finding_your_voice);
-            String message = "";
-            if (getArguments() != null) {
-                message = getArguments().getString("test1");
-            }
-                findYourVoice.setText(message);
+            getStringsForFindingYourVoice();
         } else if (view.findViewById(R.id.main_btm_sheet_resources) != null) {
-            TextView resources = view.findViewById(R.id.btm_sheet_title_Resource);
-            String message = "";
-            Log.d("text", "onCreateView: " + getArguments());
-            if (getArguments() != null) {
-                message = getArguments().getString("test2");
-            }
-            resources.setText(message);
+            getStringsForResources();
         }
         return view;
+    }
+
+    private void getStringsForWorkplaceSafety() {
+        ref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                workplaceSafety = view.findViewById(R.id.title_WorkplaceSafety);
+                workPlaceSafetyIntro = view.findViewById(R.id.content_WorkplaceSafety);
+
+                // getting the strings from firebase
+                title = dataSnapshot.child("WorkplaceSafetyTxt").getValue(String.class);
+                introduction = dataSnapshot.child("workplaceSafetyIntro").getValue(String.class);
+
+                workplaceSafety.setText(title);
+                workPlaceSafetyIntro.setText(introduction);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+    private void getStringsForFindingYourVoice() {
+        ref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                findYourVoice = view.findViewById(R.id.btm_sheet_title_finding_your_voice);
+                findYourVoiceIntro = view.findViewById(R.id.content_find_your_voice);
+                findYourVoiceBtnOne = view.findViewById(R.id.btn_one_find_your_voice);
+                findYourVoiceBtnTwo = view.findViewById(R.id.btn_two_find_your_voice);
+                findYourVoiceBtnThree = view.findViewById(R.id.btn_three_find_your_voice);
+
+                title = dataSnapshot.child("findYourVoiceTxt").getValue(String.class);
+                introduction = dataSnapshot.child("findingYourVoiceIntro").getValue(String.class);
+                findYourVoiceBtnOneString = dataSnapshot
+                                                .child("findYourVoiceBtnOne")
+                                                .getValue(String.class);
+                findYourVoiceBtnTwoString = dataSnapshot
+                                                .child("findYourVoiceBtnTwo")
+                                                .getValue(String.class);
+                findYourVoiceBtnThreeString = dataSnapshot
+                                                .child("findYourVoiceBtnThree")
+                                                .getValue(String.class);
+
+                findYourVoice.setText(title);
+                findYourVoiceIntro.setText(introduction);
+                findYourVoiceBtnOne.setText(findYourVoiceBtnOneString);
+                findYourVoiceBtnTwo.setText(findYourVoiceBtnTwoString);
+                findYourVoiceBtnThree.setText(findYourVoiceBtnThreeString);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+    private void getStringsForResources() {
+        ref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                resources = view.findViewById(R.id.btm_sheet_title_Resource);
+                resourcesIntro = view.findViewById(R.id.content_Resource);
+                resourcesAlbertaTitle = view.findViewById(R.id.title_Alberta);
+                resourcesCalgaryWorkersTitle = view.findViewById(R.id.title_calgary_workers);
+                resourcesContentContinuation = view.findViewById(R.id.content_resources_continuation);
+
+                title = dataSnapshot.child("resourcesTxt").getValue(String.class);
+                introduction = dataSnapshot.child("resourcesIntro").getValue(String.class);
+                resourcesAlbertaString = dataSnapshot
+                                                .child("resourcesAlbertaTitle")
+                                                .getValue(String.class);
+                resourcesCalgaryWorkersString = dataSnapshot
+                                                        .child("resourcesWorkersTitle")
+                                                        .getValue(String.class);
+                resourcesContentContinuationString = dataSnapshot
+                                                        .child("resourcesIntroTwo")
+                                                        .getValue(String.class);
+
+                resources.setText(title);
+                resourcesIntro.setText(introduction);
+                resourcesAlbertaTitle.setText(resourcesAlbertaString);
+                resourcesCalgaryWorkersTitle.setText(resourcesCalgaryWorkersString);
+                resourcesContentContinuation.setText(resourcesContentContinuationString);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
     }
 }
--- app/src/main/java/com/javagroup/javacapstoneapp/ViewPagerAdapter.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/ViewPagerAdapter.java
@@ -1,17 +1,11 @@
 package com.javagroup.javacapstoneapp;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     private int pageSectionNumber;
@@ -27,23 +21,15 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
         MainFragment mainFragment = new MainFragment();
         Bundle bundle = new Bundle();
 
-        // TODO: get string from firebase
-        Log.d("TESTTAG", "getItem: " + FirebaseOperations.getString());
         switch (position) {
          case 0:
              mainFragment.mainResource = R.layout.activity_mainsection_workplace_safety;
-             bundle.putString("test", "Workplace Safety");
-             mainFragment.setArguments(bundle);
              break;
          case 1:
              mainFragment.mainResource = R.layout.activity_mainsection_finding_your_voice;
-             bundle.putString("test1", "asdasdasd");
-             mainFragment.setArguments(bundle);
              break;
          case 2:
              mainFragment.mainResource = R.layout.activity_mainsection_resources;
-             bundle.putString("test2", "hello");
-             mainFragment.setArguments(bundle);
              break;
          default:
              break;
