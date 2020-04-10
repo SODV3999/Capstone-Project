dd1ce11c9f4c5886d595dee818359d55de6b5e0f
Lateef Azeez <codelate18@gmail.com>
2020-04-09 21:59:11 -0600

    Implemented new design on all bottom sheets

--- app/src/main/java/com/javagroup/javacapstoneapp/mainsection_FindingYourVoiceActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/mainsection_FindingYourVoiceActivity.java
@@ -20,23 +20,10 @@ public class mainsection_FindingYourVoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainsection_finding_your_voice);
 
-        init();
-        slideUpDown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              bsbFindingYourVoice.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-            }
-        });
 
     }
 
-    private void init() {
-        this.fyvLinearLayout = findViewById(R.id.btm_sheet_finding_your_voice);
-        this.bsbFindingYourVoice = BottomSheetBehavior.from(fyvLinearLayout);
-        this.slideUpDown = findViewById(R.id.slideUpDownFindingYourVoice);
-        mainsectionFindingYourVoiceBottomSheet = new mainsection_FindingYourVoiceBottomSheet();
 
-    }
 
 }
