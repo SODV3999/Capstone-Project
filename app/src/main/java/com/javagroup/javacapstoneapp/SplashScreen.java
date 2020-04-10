fef9e215b5f91e18fd73e84c00b0f3537575f663
Neil John Medalla <medallaneiljohn@gmail.com>
2020-04-09 21:24:00 -0600

    retrieved disclaimer content from db

--- app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.javagroup.javacapstoneapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
@@ -7,16 +8,25 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class SplashScreen extends AppCompatActivity {
 
     ImageView logo;
-    TextView school;
+    TextView school, disclaimerContent;
+
+    private DatabaseReference stringsRef = FirebaseOperations.strings();
+    private static final String TAG = "SplashScreen";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +70,8 @@ public class SplashScreen extends AppCompatActivity {
     public void showDisclaimerDialog() {
         final Dialog disclaimerDialog = new Dialog(this);
         disclaimerDialog.setContentView(R.layout.fragment_dialog_disclaimer);
+        disclaimerContent = disclaimerDialog.findViewById(R.id.txt_context);
+        getDisclaimerStrings();
         Button btn_agree = (Button) disclaimerDialog.findViewById(R.id.btn_agree);
         ImageButton btn_close = (ImageButton) disclaimerDialog.findViewById(R.id.btn_close);
         btn_agree.setOnClickListener(new View.OnClickListener() {
@@ -82,5 +94,20 @@ public class SplashScreen extends AppCompatActivity {
         disclaimerDialog.setCancelable(false);
         disclaimerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         disclaimerDialog.show();
+    }
+
+    private void getDisclaimerStrings() {
+        stringsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String content = dataSnapshot.child("disclaimerContent").getValue(String.class);
+                disclaimerContent.setText(content);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
     }
 }
