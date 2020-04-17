76ffe8e66bef3fca6323fffbd71d78ee834cdb14
Bruno Mendonca <me@brunomendonca.com>
2020-04-17 15:46:04 -0600

    Modify Nigam's Section: Employment Standards and HumanRights

--- app/src/main/java/com/javagroup/javacapstoneapp/subsection_WorkplaceSafety_HumanRights.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/subsection_WorkplaceSafety_HumanRights.java
@@ -1,14 +1,19 @@
 package com.javagroup.javacapstoneapp;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+public class subsection_WorkplaceSafety_HumanRights extends
+        AppCompatActivity {
 
-public class mainsection_WorkplaceSafetyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainsection_workplace_safety);
+        setContentView(R.layout.activity_subsection_workplacesafety_humanrights);
+
     }
+
+
 }
