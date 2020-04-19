a80fd05b9a9e921b9012d0eebf7c2770d281e6ea
Sagar Soni <s.soni247@mybvc.ca>
2020-04-18 22:20:52 -0600

    resource page link open in application by sagar and nigam

--- app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
+++ app/src/main/java/com/javagroup/javacapstoneapp/MainActivity.java
@@ -1,5 +1,14 @@
 package com.javagroup.javacapstoneapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
 import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
@@ -9,39 +18,24 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
-
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CALL = 1;
+    private static final int REQUEST_CALL = 1 ;
+    private ConstraintLayout navigationScreen, browserContainer;
     ImageButton openNav;
-    TabLayout tabLayout;
-    WebView webView;
-    private ConstraintLayout navigationScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
         /*WRITTEN BY: Victor Charl Corpuz*/
         navigationScreen = findViewById(R.id.navigationScreen);
+        browserContainer = findViewById(R.id.browserContainer);
         openNav = findViewById(R.id.openNav);
 
         openNav.setOnClickListener(new View.OnClickListener() {
@@ -66,25 +60,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tabLayout = findViewById(R.id.tab_layout);
-
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(
                 new ViewPagerAdapter(getSupportFragmentManager(),
-                        3,
-                        3));
+                3,
+                3));
 
         /*Written by: Victor Charl*/
         Bundle bundle = getIntent().getExtras();
         int viewPagerPage = 0;
-        if (bundle != null) { //check if the bundle is not null and store it in viewPagerPage
-            // variable
+        if(bundle != null){ //check if the bundle is not null and store it in viewPagerPage variable
             viewPagerPage = bundle.getInt("position");
         }
         /*setting viewPagerPage
             by default setCurrentItem == 0*/
         viewPager.setCurrentItem(viewPagerPage);
-        tabLayout.setupWithViewPager(viewPager, true);
     }
 
     public void launchOccupationalHealthAndSafety(View view) {
@@ -93,11 +83,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchEmploymentStandards(View view) {
-        startActivity(new Intent(this, subsection_WorkplaceSafety_EmploymentStandards.class));
+        startActivity(new Intent(this, old_EmploymentStandardsActivity.class));
     }
 
     public void launchHumanRights(View view) {
-        startActivity(new Intent(this, subsection_WorkplaceSafety_HumanRights.class));
+        startActivity(new Intent(this, old_HumanRightsActivity.class));
     }
 
     public void launchFunders(View view) {
@@ -111,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchCaseStudy1(View view) {
         startActivity(new Intent(this,
-                CaseStudy1.class));
+               CaseStudy1.class));
     }
 
     public void launchCaseStudy2(View view) {
@@ -124,50 +114,54 @@ public class MainActivity extends AppCompatActivity {
                 CaseStudy3.class));
     }
 
-
-    public void calling_one(View view)
-    {
+    public void calling_one(View view) {
         makePhoneCall_1();
-    }
+   }
 
-    private void makePhoneCall_1() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-        } else {
+    private  void makePhoneCall_1()
+    {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+        }
+        else
+        {
             // String dial ="tel:" +5879692301;
             //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
-            String dial = "780-486-9009";
+            String dial ="780-486-9009";
             Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + dial));
+            intent.setData(Uri.parse("tel:"+dial));
             startActivity(intent);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CALL) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (requestCode == REQUEST_CALL){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
                 makePhoneCall_1();
                 makePhoneCall_2();
-            } else {
-                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"Permission DENIED", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void makePhoneCall_2() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+    private  void makePhoneCall_2()
+    {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+        {
 
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+        }else
+        {
             // String dial ="tel:" +5879692301;
             //startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
-            String dial = "403-264-8100";
+            String dial ="403-264-8100";
             Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + dial));
+            intent.setData(Uri.parse("tel:"+dial));
             startActivity(intent);
         }
     }
@@ -176,19 +170,7 @@ public class MainActivity extends AppCompatActivity {
         makePhoneCall_2();
     }
 
-    public void goToUrl_one(View view) {
-        String url = "https://workershealthcentre.ca/";
-        Uri uriUrl = Uri.parse(url);
-        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-        startActivity(launchBrowser);
-    }
 
-    public void goToUrl_two(View view) {
-        String url = "http://www.helpwrc.org/our-board/";
-        Uri uriUrl = Uri.parse(url);
-        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-        startActivity(launchBrowser);
-    }
 
     public void alberta_emp_standards(View view) {
         String url = "https://www.alberta.ca/alberta-employment-standards-rules.aspx";
@@ -196,14 +178,12 @@ public class MainActivity extends AppCompatActivity {
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
     }
-
     public void alberta_act(View view) {
         String url = "http://www.qp.alberta.ca/documents/Acts/E09.pdf";
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
     }
-
     public void emp_standard_regulation(View view) {
         String url = "http://www.qp.alberta.ca/documents/Regs/1997_014.pdf";
         Uri uriUrl = Uri.parse(url);
@@ -219,12 +199,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void hr_commission(View view) {
-        String url = "https://www.albertahumanrights.ab.ca/Documents/GuideProcess_Complainants" +
-                ".pdf)";
+        String url = "https://www.albertahumanrights.ab.ca/Documents/GuideProcess_Complainants.pdf)";
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
     }
+
+    private void openingLink(String url)
+    {
+        browserContainer.setTranslationY(3000);
+        browserContainer.animate().translationYBy(-3000).setDuration(700);
+        final FragmentTransaction openTheBrowser = getSupportFragmentManager().beginTransaction();
+        final WebBrowserActivity webBrowserActivity = new WebBrowserActivity();
+        Bundle link = new Bundle();
+        link.putString("url", url);
+        webBrowserActivity.setArguments(link);
+        openTheBrowser.add(R.id.browserContainer, webBrowserActivity);
+        openTheBrowser.commit();
+    }
+
+    public void showBrowser(View view){
+        switch (view.getId())
+        {
+            case R.id.workershealthcentre:
+                openingLink("https://workershealthcentre.ca/");
+            case R.id.helpwrc:
+                openingLink("https://www.helpwrc.org/our-board/");
+        }
+    }
+
 
 
     /*WRITTEN BY : VICTOR CHARL CORPUZ*/
@@ -249,24 +252,6 @@ public class MainActivity extends AppCompatActivity {
         confirmDialog.setCancelable(false);
         confirmDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         confirmDialog.show();
-    }
-
-    public void collapseNavFromHere(View view) {
-        switch (view.getId()) {
-            case R.id.main_section_workplace_safety:
-            case R.id.main_btm_sheet_finding_your_voice:
-            case R.id.main_btm_sheet_resources:
-            case R.id.btm_sheet_finding_your_voice:
-            case R.id.btm_sheet_workplace_safety:
-            case R.id.btm_sheet_resources:
-            case R.id.btmsheet_content_resources:
-            case R.id.bottomsheet_content_workplacesafety:
-                navigationScreen.animate().translationYBy(-3000)
-                        .translationXBy(-3000).setDuration(450);
-                break;
-
-        }
-
     }
 
 }
